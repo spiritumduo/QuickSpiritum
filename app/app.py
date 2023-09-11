7addff4a29b3a354d316f20e2645aac42e337bd7
cotswoldsmaker <mark.allan.bailey@gmail.com>
2023-09-11 21:21:04 +0100

    round 2

---
+++ app/app.py
@@ -0,0 +1,19 @@
+
+# import flask module
+from flask import Flask
+ 
+# instance of flask application
+app = Flask(__name__)
+ 
+# home route that returns below text
+# when root url is accessed
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/me")
+def hello_world2():
+    return "<p>Hello, World!2</p>"
+ 
+if __name__ == '__main__':
+    app.run(debug=True, port=1111)
